56edfd23e1a4c7a98ef30e52e2a659e59b95ac5f
DESKTOP-CNUE7RF\Luc Hoang <hoanglucmc@gmail.com>
2017-03-16 15:45:57 +0700

    update

---
+++ app/controllers/login.controller.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('gApp.login')
+        .controller('LoginController', LoginController);
+        LoginController.$inject = ['$state','$scope'];
+
+    function LoginController($state, $scope) {
+        var vm = this;
+
+        vm.show = function(){
+            console.log("Success!");
+        };
+
+    }
+    
+})();
